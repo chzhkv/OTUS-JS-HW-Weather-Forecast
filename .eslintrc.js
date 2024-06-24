adaf30419d780f183f184ef8a32dc0d4c7e39ce3
chzhkv <chizhikvo@gmail.com>
2024-06-24 15:48:51 +0400

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest/globals": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "max-len": ["error", { "ignoreComments": true }]
-    },
-    "plugins": ["jest"],
-}
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "max-len": ["error", { ignoreComments: true }],
+  },
+  plugins: ["jest"],
+};
