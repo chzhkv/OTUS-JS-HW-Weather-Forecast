d84887c6d84ced4255fe90a9ff96bdbfa948950d
chzhkv <chizhikvo@gmail.com>
2024-07-21 18:32:03 +0400

    city name error handler

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,8 +1,15 @@
 import { API_KEY_OPENWEATHER } from "./constants";
 
 export async function getWeather(cityName) {
-  let data = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${API_KEY_OPENWEATHER}`,
-  );
-  return data.json();
+  try {
+    let data = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${API_KEY_OPENWEATHER}`,
+    );
+    return data.json();
+  } catch (err) {
+    alert(
+      err,
+      "Unfortunately, we cannot find this city. Please enter the correct city name",
+    );
+  }
 }
